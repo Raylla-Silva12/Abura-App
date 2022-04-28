2091fcc64fb89de9b542c56ecb8f88ddf4d37690
Raylla-Silva12 <raylla.l.silva@gmail.com>
2022-04-28 17:47:55 -0300

    Versão 0.4

--- App.js
+++ App.js
@@ -1,23 +1,79 @@
-import * as React from 'react';
-import { Button, View, Text, StyleSheet, ScrollView, Image, ImageBackground } from 'react-native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { NavigationContainer } from '@react-navigation/native';
+import * as React from "react";
+import {
+  useState,
+  Button,
+  Alert,
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  Image,
+  ImageBackground,
+} from "react-native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { NavigationContainer } from "@react-navigation/native";
 
 function HomeScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button
-        onPress={() => navigation.navigate('Setting')}
-        title="Ir para configurações"
-      />
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#FFF",
+      }}
+    >
+      <View style={{ width: 240 }}>
+        <Button
+          /* onPress={() => navigation.navigate('Configurações')} */
+          title="CHAMAR SAMU"
+          color="#eb8dd6"
+        />
+      </View>
+
+      <View style={{ marginTop: 30, width: 240 }}>
+        <Button
+          /* onPress={simpleAlert} */
+          color="#8be1e3"
+          width="200"
+          title="EXIBIR UNIDADES PRÓXIMAS"
+        />
+      </View>
     </View>
   );
 }
 
 function SettingScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button onPress={() => navigation.goBack()} title="Voltar para home" />
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#FFF",
+      }}
+    >
+      <Button onPress={() => navigation.goBack()} title="Voltar para Início"
+        color="#FF1493"
+        width="200"
+      />
+    </View>
+  );
+}
+
+function AboutScreen({ navigation }) {
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#FFF",
+        fontSize: 50,
+        padding: 28
+      }}
+    >
+      <Text style={{fontSize: 20, textAlign: "center"}}>Assistência e assertividade para os momentos em que mais precisar!</Text>
     </View>
   );
 }
@@ -28,10 +84,22 @@ export default function App() {
   return (
     <NavigationContainer>
       <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="Home" component={HomeScreen} />
-        <Drawer.Screen name="Setting" component={SettingScreen} />
+        <Drawer.Screen name="Início" component={HomeScreen} />
+        <Drawer.Screen name="Configurações" component={SettingScreen} />
+        <Drawer.Screen name="Sobre" component={AboutScreen} />
       </Drawer.Navigator>
     </NavigationContainer>
   );
 }
 
+    /*<ScrollView>
+        <ImageBackground
+        style={{
+          width: undefined,
+          padding: 16,
+          paddingTop: 48
+
+        }}>
+          <Text>Raylla S.</Text>
+        </ImageBackground>
+      </ScrollView>*/
